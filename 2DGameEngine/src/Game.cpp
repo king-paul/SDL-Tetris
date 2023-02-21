f2dfa8394bfc7019b6419b5d761579d18469d556
Paul King <PaulKing09@bigpond.com>
2023-02-21 14:36:07 +1100

    Updated keyboard input and added colour pallete

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -64,7 +64,7 @@ void Game::Initialize(int width, int height)
 void Game::ProcessInput()
 {
 	SDL_Event event;
-	SDL_PollEvent(&event); 
+	SDL_PollEvent(&event);
 
 	switch (event.type)
 	{
@@ -81,33 +81,51 @@ void Game::ProcessInput()
 		}
 
 		// sets the running state to false when the ESC key is pressed
+
 		case SDL_KEYDOWN: {
-			if (event.key.keysym.sym == SDLK_ESCAPE) {
-				isRunning = false;
+
+			switch (event.key.keysym.sym)
+			{
+				case SDLK_ESCAPE:
+					isRunning = false;
+				break;
+
+				case SDLK_LEFT: case SDLK_a:
+					game->MovePieceLeft();
+				break;
+
+				case  SDLK_RIGHT: case SDLK_d:
+					game->MovePieceRight();
+				break;
+
+				case SDLK_DOWN: case SDLK_s:
+					game->MovePieceDown();
+				break;
+
+				case SDLK_UP: case SDLK_w:
+					if (!keyPressed) // restrict rotation to once per key press					
+						game->RotateCurrentPiece();								
+				break;
+
+				case SDLK_g:
+					if (!keyPressed)
+						showGrid = !showGrid;
+				break;
 			}
 
-			if (event.key.keysym.sym == SDLK_LEFT)
-			{				
-				game->MovePieceLeft();
-			}
+			keyPressed = true;
 
-			if (event.key.keysym.sym == SDLK_RIGHT)
-			{				
-				game->MovePieceRight();
-			}
-
-			if (event.key.keysym.sym == SDLK_DOWN)
-			{				
-				game->MovePieceDown();
-			}
-
-			if (event.key.keysym.sym == SDLK_UP)
-			{				
-				game->RotateCurrentPiece();
-			}
-
-			break;
+		break;
 		}
+
+		case SDL_KEYUP: {
+
+			keyPressed = false;
+
+			//if (event.key.keysym.sym == SDLK_UP)
+				//rotatePressed = false;
+		}
+
 		default: {
 			
 		}
@@ -142,13 +160,12 @@ void Game::Render()
 	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255); // set the background colour
 	SDL_RenderClear(renderer); // clears the back render buffer
 	
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // set render colour to white	
+	SetRenderColor(Color::WHITE);
 
-	// draw field
-	SDL_RenderDrawRect(renderer, &border);
-
-	// draw field
+	/* draw graphics on screen */
 	DrawBoard();
+	DrawCurrentPiece();
+	/* end of graphics draw */
 
 	SDL_RenderPresent(renderer); // renders the frame in on the window
 }
@@ -162,31 +179,83 @@ void Game::Destroy()
 	SDL_Quit(); // closes the program
 }
 
+void Game::SetRenderColor(Color color)
+{
+	switch (color)
+	{
+		case Color::BLACK: SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			break;
 
+		case Color::RED: SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			break;
+
+		case Color::LIME: SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			break;
+
+		case Color::BLUE: SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+			break;
+
+		case Color::YELLOW: SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+			break;
+
+		case Color::MAGENTA: SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+			break;
+
+		case Color::CYAN: SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+			break;
+
+		case Color::GRAY: SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+			break;
+
+		case Color::MAROON: SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+			break;
+
+		case Color::GREEN: SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+			break;
+
+		case Color::NAVY: SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255);
+			break;
+
+		case Color::OLIVE: SDL_SetRenderDrawColor(renderer, 128, 128, 0, 255);
+			break;
+
+		case Color::PURPLE: SDL_SetRenderDrawColor(renderer, 128, 0, 128, 255);
+			break;
+
+		case Color::TEAL: SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
+			break;
+
+		case Color::WHITE: SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			break;
+	}
+
+}
 
 void Game::DrawBoard()
 {
-	int count = 0;
-	Tetromino* piece = game->GetCurrentPiece();
-
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-	// hoirzontal lines
-	for (int y = 0; y < ROWS; y++)
+	if (showGrid)
 	{
-		SDL_RenderDrawLine(renderer, GRID_OFFSET_X + 1, GRID_OFFSET_Y + y * TILE_SIZE,
-			GRID_OFFSET_X - 1 + TILE_SIZE * COLS, GRID_OFFSET_Y + y * TILE_SIZE);
-	}
+		SetRenderColor(Color::MAROON);
 
-	//vertical lines
-	for (int x = 0; x < COLS; x++)
-	{
-		SDL_RenderDrawLine(renderer, GRID_OFFSET_X + x * TILE_SIZE, GRID_OFFSET_Y + 1,
-			GRID_OFFSET_X + x * TILE_SIZE, GRID_OFFSET_Y - 1 + ROWS * TILE_SIZE);
+		// hoirzontal lines
+		for (int y = 0; y < ROWS; y++)
+		{
+			SDL_RenderDrawLine(renderer, GRID_OFFSET_X + 1, GRID_OFFSET_Y + y * TILE_SIZE,
+				GRID_OFFSET_X - 1 + TILE_SIZE * COLS, GRID_OFFSET_Y + y * TILE_SIZE);
+		}
+
+		//vertical lines
+		for (int x = 0; x < COLS; x++)
+		{
+			SDL_RenderDrawLine(renderer, GRID_OFFSET_X + x * TILE_SIZE, GRID_OFFSET_Y + 1,
+				GRID_OFFSET_X + x * TILE_SIZE, GRID_OFFSET_Y - 1 + ROWS * TILE_SIZE);
+		}
 	}
 
 	// draw bordered squares
-	SDL_SetRenderDrawColor(renderer, 0, 200, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 127, 127, 127, 255); // grey
 	for (int y = 0; y < ROWS; y++)
 	{
 		for (int x = 0; x < COLS; x++)
@@ -199,6 +268,15 @@ void Game::DrawBoard()
 		}
 	}
 
+}
+
+void Game::DrawCurrentPiece()
+{
+	Tetromino* piece = game->GetCurrentPiece();
+	int count = 0;
+
+	// draw the current piece
+
 	for (int y = 0; y < piece->size; y++)
 	{
 		for (int x = 0; x < piece->size; x++)
@@ -206,7 +284,7 @@ void Game::DrawBoard()
 			if (piece->tiles[y][x] == true)
 			{
 				tetromino[count] = { piece->posX * TILE_SIZE + (x * TILE_SIZE) + GRID_OFFSET_X,
-									 piece->posY * TILE_SIZE + (y * TILE_SIZE) + GRID_OFFSET_Y+1,
+									 piece->posY * TILE_SIZE + (y * TILE_SIZE) + GRID_OFFSET_Y + 1,
 									 TILE_SIZE, TILE_SIZE };
 				count++;
 			}
@@ -215,18 +293,17 @@ void Game::DrawBoard()
 
 	switch (piece->type)
 	{
-		case 0: SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); break; // red
-		case 1: SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); break; // green
-		case 2: SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); break; //blue
-		case 3: SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); break; // yellow
-		case 4: SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255); break; // 
-		case 5: SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255); break; // 
-		case 6: SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); break; // white
+		case 0: SetRenderColor(Color::RED); break;
+		case 1: SetRenderColor(Color::GREEN); break;
+		case 2: SetRenderColor(Color::BLUE); break;
+		case 3: SetRenderColor(Color::YELLOW); break;
+		case 4: SetRenderColor(Color::MAGENTA); break;
+		case 5: SetRenderColor(Color::CYAN); break;
+		case 6: SetRenderColor(Color::PURPLE); break;
 	}
 
 	for (auto square : tetromino)
 	{
 		SDL_RenderFillRect(renderer, &square);
 	}
-
 }
--- 2DGameEngine/src/Game.h
+++ 2DGameEngine/src/Game.h
@@ -13,6 +13,25 @@ const int ROWS = 18;
 const int GRID_OFFSET_X = 6;
 const int GRID_OFFSET_Y = 12;
 
+enum class Color
+{
+	BLACK = 0,
+	RED = 9,
+	LIME = 10,
+	BLUE = 12,
+	YELLOW = 11,
+	MAGENTA = 13,
+	CYAN = 14,
+	GRAY = 8,
+	MAROON = 1,
+	GREEN = 2,
+	NAVY = 4,
+	OLIVE = 3,
+	PURPLE = 5,
+	TEAL = 6,
+	WHITE = 15
+};
+
 class Game 
 {
 private:
@@ -22,10 +41,19 @@ private:
 	Tetris* game;
 
 	// game objects
-	SDL_Rect border = { GRID_OFFSET_X, GRID_OFFSET_Y, COLS * TILE_SIZE, ROWS * TILE_SIZE };
+	//SDL_Rect border = { GRID_OFFSET_X, GRID_OFFSET_Y, COLS * TILE_SIZE, ROWS * TILE_SIZE };
 	SDL_Rect tetromino[4]; 
 
+	//keyboard pressed
+	bool keyPressed = false;
+
+	// drawing options
+	bool showGrid = false;
+
 	void DrawBoard();
+	void DrawCurrentPiece();
+
+	void SetRenderColor(Color color);
 
 public:	
 	Game();
